e951d86d55b542067f9b22827117a95b163df906
Tyler Campbell <tcampb30@gmail.com>
2018-01-12 17:54:44 -0500

    Added jwt and bcrypt-js

--- models/model/user.js
+++ models/model/user.js
@@ -1,38 +1,41 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../db');
-
-
+const bcrypt = require('bcryptjs');
 const User = sequelize.define('user', {
     firstName: {
-        type: Sequelize.STRING, notNull: true
+        type: Sequelize.STRING
     },
     lastName: {
-        type: Sequelize.STRING, notNull: true
+        type: Sequelize.STRING
     },
     email: {
         type: Sequelize.STRING,
-        validate: {isEmail:true,notNull: true }
+        validate: {isEmail:true }
     },
     password: {
         type:Sequelize.STRING
     },
     primary_token:{
-        type: Sequelize.STRING, notNull: true
+        type: Sequelize.STRING
     },
     google_token:{
-        type: Sequelize.STRING, notNull: true
+        type: Sequelize.STRING
     },
     sf_token :{
-        type: Sequelize.STRING, notNull: true
+        type: Sequelize.STRING
     }
-
-
   });
 
-  
-  User.sync()
-  .then(() => {
-      console.log('Created User table!');
-    });
-    
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash("test123", salt, (err, hash) => {
+                User.sync()
+                .then(
+                    User.create({
+                        firstName: 'Tyler',
+                        lastName: 'test',
+                        email: 'test2@test.com',
+                        password: hash
+                    }))
+                })
+                })
     module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const router = express.Router();
-const user = require('../models/model/user');
-// const jwt = require('jsonwebtoken');
+const userModel = require('../models/model/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('../config.json');
 const _ = require('lodash');
+
+const veryifyPassword = (password, user) => {
+  bcrypt.compare(password, user.dataValues.password, (err, validation) => {
+    if (err) return err;
+    if (validation) return generateAuthToken(user);
+    return Error("Invalid password");
+  })
+};
+
+const generateAuthToken = (user) => {
+  let userId = user.dataValues.id;
+  let access = 'auth';
+  let token = jwt.sign({id: userId, access}, config.secret).toString();
+  
+  return user.update({
+    primary_token: token
+  }).save().then(() => {
+    return token;
+  });
+
+}
 
 /* GET users listing. */
 router.post('/', function(req, res) {
   let {email, password} = _.pick(req.body, ['email', 'password']);
-  
-  res.end();
-});
+  userModel.findOne({
+    where: {email: `${email}`}
+  }).then((user) => veryifyPassword(password, user)).then((token) => {
+    res.header('x-auth', token).send("Success")
+  }).catch((err) => {
+    res.status(400).send(err)
+   })
+  });
+
+
 
 module.exports = router;

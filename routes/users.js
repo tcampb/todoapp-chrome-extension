6d4ae79b719d958abe4ab28d084cf833cf928d91
Tyler Campbell <tcampb30@gmail.com>
2018-01-12 22:00:54 -0500

    Updated header to include token

--- routes/users.js
+++ routes/users.js
@@ -6,38 +6,46 @@ const jwt = require('jsonwebtoken');
 const config = require('../config.json');
 const _ = require('lodash');
 
-const veryifyPassword = (password, user) => {
-  bcrypt.compare(password, user.dataValues.password, (err, validation) => {
+const verifyPassword = (password, user) => {
+  return bcrypt.compare(password, user.dataValues.password).then((res) => {
+    if (res) return user;
     if (err) return err;
-    if (validation) return generateAuthToken(user);
-    return Error("Invalid password");
+    return Error("Fail");
   })
-};
-
+}
 const generateAuthToken = (user) => {
   let userId = user.dataValues.id;
   let access = 'auth';
-  let token = jwt.sign({id: userId, access}, config.secret).toString();
-  
-  return user.update({
-    primary_token: token
-  }).save().then(() => {
-    return token;
-  });
-
+  let token = jwt.sign({id: userId, access}, config.secret);
+  userModel.update(
+    {primary_token: token},
+    {where: {
+      id: userId
+    }
+  })
+  return token;
 }
 
 /* GET users listing. */
 router.post('/', function(req, res) {
   let {email, password} = _.pick(req.body, ['email', 'password']);
   userModel.findOne({
-    where: {email: `${email}`}
-  }).then((user) => veryifyPassword(password, user)).then((token) => {
+    where: {email}
+  })
+  .then((user) => {
+    return verifyPassword(password, user);
+  })
+  .then((response) => {
+    return generateAuthToken(response);
+  })
+  .then((token) => {
     res.header('x-auth', token).send("Success")
-  }).catch((err) => {
+  })
+  .catch((err) => {
     res.status(400).send(err)
    })
-  });
+  })
+  
 
 
 

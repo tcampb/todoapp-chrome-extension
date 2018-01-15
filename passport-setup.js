a998d61e19504b2361fabb826d7eff4435c9cbc4
Tyler Campbell <tcampb30@gmail.com>
2018-01-15 13:10:21 -0500

    Added passport.serializeUser method

--- passport-setup.js
+++ passport-setup.js
@@ -2,25 +2,46 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const config = require('./config');
 const userModel = require('./models/table/user');
-const _ = require('lodash');
+
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    userModel.findById(id).then((user) => {
+        done(null, user);
+    })
+});
 
 module.exports = passport.use(new GoogleStrategy({
     clientID: config.clientIdGoogle,
     clientSecret: config.clientSecretGoogle,
     callbackURL: '/users/google/redirect' 
     }, (accessToken, refreshToken, profile, done) => {
-        let fullName = profile.displayName.split(' ');
-        userModel.create({
-            firstName: fullName[0],
-            lastName: fullName[1],
-            email: config.testEmail,
-            googleId: profile.id
+        //Check if user already exists in our db
+        userModel.findOne({
+            where: {
+                googleId: profile.id
+            }
         })
+        .then((currentUser) => {
+            if (currentUser) {
+                throw Error("User already exists");
+            } else {
+                let fullName = profile.displayName.split(' ');
+                userModel.create({
+                    firstName: fullName[0],
+                    lastName: fullName[1],
+                    email: config.testEmail,
+                    googleId: profile.id
+                })
         .then((newUser) => {
-            console.log("New user created");
-            res.send("Success");
-        });
-    })
-    );
+            done(null, newUser);
+        })
+    }
+})
+})
+);
 
  

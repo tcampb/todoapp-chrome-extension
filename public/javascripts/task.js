79eb328166bf042b6e598024e940870f7f2ad8fc
Tyler Campbell <tcampb30@gmail.com>
2018-01-19 01:33:59 -0500

    Updated form to include placeholder values

--- public/javascripts/task.js
+++ public/javascripts/task.js
@@ -15,6 +15,12 @@ $('[data-contact-hover]')
   });
 
 $(document).ready(() => {
+    let title = $('[data-title]').text();
+    let description = $('[data-description]').text();
+    description = description.substring(description.indexOf(':') + 2);
+    let location = $('[data-location]').text();
+    let contacts = $('[data-contact]').text();
+    let dueDate = $('[data-due-date]').text();
     $('[data-create-contact]').on('click',(event)=>{
         event.preventDefault();
         let data = {
@@ -59,7 +65,43 @@ $(document).ready(() => {
         duration: 2000
       })
       setTimeout(() => {
-        location.replace('/');
+        console.log(1);
+        window.location.replace('/');
       }, 1000);
+      });
+
+    $('[data-update]').on('click', (event) => {
+      event.preventDefault();
+      $.get('/dashboard/tasks', (data) => {
+        let formStart = data.indexOf('<form');
+        let formEnd = data.indexOf('</form>') + 7;
+        let form = data.substring(formStart, formEnd);
+        $('[data-content]').replaceWith(form);
       })
+      .then(() => {
+        $('form').attr({'style':'margin-top: 20px'});
+        $('[name="title"]').val(title);
+        $('[name="content"]').val(description);
+        $('[name="enddate"]').val(dueDate);
+        $('[name="title"]').val(title);
+        $('[data-label]').addClass('hide');
+        $('[data-submit]').val('Save');
+        $('[data-contact-dropdown')
+        .dropdown();
+        $('[data-calendar]').calendar();
+        $('[data-contact-hover]')
+          .popup({
+            inline: true,
+            hoverable:true,
+            delay: {
+              show:300,
+              hide:500
+            }
+        });
+      })
+      
+
+    });
 });
+
+

b45bb2adb550e58140e92cc4fdcc64ca7022d86b
Tyler Campbell <tcampb30@gmail.com>
2018-01-20 15:17:05 -0500

    Added contacts page

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,24 +4,23 @@ const getTasks = require('../models/query/getTask');
 const update = require('../models/query/update');
 const moment = require('moment');
 
-router.get('/',(req,res,next)=>{
-    try{
-    getTasks.find_all_contact(res.locals.user).then(allContacts => {
-        const contact = allContacts.map(contact => {
+router.get('/', (req, res) => {
+    getTasks.find_all_contact(res.locals.user)
+    .then(allContacts => {
+        if (!allContacts) return allContacts;
+        return allContacts.map(contact => {
             return { 
                 id:contact.id,
                 name:contact.firstName+" "+contact.lastName
             }
         })
-        res.render('contact',{
+    })
+    .then((contact) => {
+        res.render('contact', {
             contact: contact,
             document: 'contact'
         });
     })
-        
-    }catch(err){
-        res.end(err);
-    }
 })
 .post('/', (req, res, next) => {
     const body = req.body;

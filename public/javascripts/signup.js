1a5fce995870d85c9858958088a919634563bd38
Tyler Campbell <tcampb30@gmail.com>
2018-01-22 15:33:09 -0500

    User can now create an account using gmail / password

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -3,12 +3,22 @@ const googleAuth = (email) => {
     $('[data-continue]').attr({'style': 'display:none;'});
     $('[data-email]').addClass('hide');
     $('[data-google]').removeClass('hide');
+    $('[data-sign-up]').html('Prefer to create an account with a password? <a href="#" data-switch-password>Click here</a>')
+    $('[data-switch-password]').on('click', (event) => {
+        console.log("wdawdaw");
+        event.preventDefault();
+        emailAuth(null);
+    })
 }
 
 const emailAuth = (email) => {
+
+    if (!email) {$('[data-google]').addClass('hide')}
+
     $('[data-firstName]').removeClass('hide').attr('required');
     $('[data-lastName]').removeClass('hide').attr('required');
     $('[data-password]').removeClass('hide').attr('required');
+    $('[data-email]').removeClass('hide').attr('required');
     $('.sign-up').addClass('hide');
     $('button').attr({'style': 'margin-top: 0px'});
     $('form').attr('action', '/auth');
@@ -33,8 +43,4 @@ $(document).ready(() => {
         })
     })
 
-    $('[data-email-auth').on('click', (event) => {
-        event.preventDefault();
-
-    })
 })

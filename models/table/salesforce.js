b95470a78c672c703908206af88301949c69abca
Tyler Campbell <tcampb30@gmail.com>
2018-01-24 00:38:17 -0500

    Fixed null pointer error

--- models/table/salesforce.js
+++ models/table/salesforce.js
@@ -1,6 +1,7 @@
 const jsforce = require('jsforce');
 const async = require('async');
 
+
 exports.sfConn = (user) => {
     return new Promise(resolve => {
         const conn = new jsforce.Connection({ instanceUrl: user.sf_instance, accessToken : user.sf_token });
@@ -17,48 +18,42 @@ exports.find_sf_tasks = (user, conn) => {
         let taskArray = result.records;
         let updatedArray = [];
 
-        async.map(taskArray, (task, next) => {
-            console.log("wdaw");
-            task.enddate = task.ActivityDate;
-            task.title = task.Subject;
-            task.Description ? task.content = task.Description : task.content = '';
-            task.id = task.Id;
-            updatedArray.push(task);
-            next();
-        }, function(err){
-            resolve(updatedArray);
+        async.each(taskArray, (task, next) => {
+            !task.WhoId ? task.WhoId = '' : task.WhoId;
+            console.log(task.WhoId);
+            if (task.WhoId.startsWith('00Q')) {
+                console.log(1);
+                conn.query(`SELECT id, FirstName, LastName FROM Lead WHERE id = '${task.WhoId}'`, function(err, result){
+                    task.name = result.records[0].FirstName + ' ' + result.records[0].LastName;
+                    task.enddate = task.ActivityDate;
+                    task.title = task.Subject;
+                    task.Description ? task.content = task.Description : task.content = '';
+                    task.id = task.Id;
+                    updatedArray.push(task);
+                    next();
+                })
+            } else if (task.WhoId.startsWith('003')) {
+                conn.query(`SELECT id, FirstName, LastName FROM Contact WHERE id = '${task.WhoId}'`, function(err, result){
+                    task.name = result.records[0].FirstName + ' ' + result.records[0].LastName;
+                    task.enddate = task.ActivityDate;
+                    task.title = task.Subject;
+                    task.Description ? task.content = task.Description : task.content = '';
+                    task.id = task.Id;
+                    updatedArray.push(task);
+                    next();
+                })
+            } else {
+                task.enddate = task.ActivityDate;
+                task.title = task.Subject;
+                task.Description ? task.content = task.Description : task.content = '';
+                task.id = task.Id;
+                updatedArray.push(task);
+                next();
+            }
+        }, function(err) {
+            if (err) {console.log(err)}
+            else {resolve(updatedArray)}
         })
-        // async.each(taskArrayRel, (task, next) => {
-        //     if (task.WhoId.startsWith('00Q')) {
-        //         conn.query(`SELECT id, FirstName, LastName FROM Lead WHERE id = '${task.WhoId}'`, function(err, result){
-        //             task.name = result.records[0].FirstName + ' ' + result.records[0].LastName;
-        //             task.enddate = task.ActivityDate;
-        //             task.title = task.Subject;
-        //             task.Description ? task.content = task.Description : task.content = '';
-        //             task.id = task.Id;
-        //             updatedArray.push(task);
-        //             next();
-        //         })
-        //     } else if (task.WhoId.startsWith('003')) {
-        //         conn.query(`SELECT id, FirstName, LastName FROM Contact WHERE id = '${task.WhoId}'`, function(err, result){
-        //             task.name = result.records[0].FirstName + ' ' + result.records[0].LastName;
-        //             task.enddate = task.ActivityDate;
-        //             task.title = task.Subject;
-        //             task.Description ? task.content = task.Description : task.content = '';
-        //             task.id = task.Id;
-        //             updatedArray.push(task);
-        //             next();
-        //         })
-        //     }
-          
-        // }, function(err) {
-        //     if (err) {
-        //         console.log(err);
-        //         return []}
-        //     else {
-        //         console.log("SUCCESS");
-        //         resolve(updatedArray)}
-        // })
     })
     })
 }
